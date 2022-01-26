250ba9e658f84a34bf8fa49f23387789636e0434
RomanW05 <51992135+RomanW05@users.noreply.github.com>
2022-01-26 15:51:05 +0100

    Update redirect_to_website.php

--- php/redirect_to_website.php
+++ php/redirect_to_website.php
@@ -1,13 +1,17 @@
 <?php
-$db_username = 'domaiyxq';
-$db_password = 'QPDub04CXAW3';
-$db_databe_name = 'domaiyxq_lovelydoggos';
-$db_host = 'server282.web-hosting.com';
+// Credentials
+$db_username = 'DATABASE_USERNAME';
+$db_password = 'DATABASE_PASSWORD';
+$db_databe_name = 'DATABASE_NAME';
+$db_host = 'DATABASE_HOST';
+
+// Connect to database and exit if it fails
 $conn = new mysqli($db_host, $db_username, $db_password, $db_databe_name);
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
+// Add data to the database
 $date = date("Y-m-d-h-i");
 $hashed = mysqli_real_escape_string($conn, $_GET['hashed']);
 $newsletter = mysqli_real_escape_string($conn, $_GET['newsletter']);
@@ -15,5 +19,7 @@ $sql = "INSERT INTO click_banner (hashed, newsletter, time_stamp) VALUES('$hashe
 $result = $conn->query($sql);
 $conn->close();
 
-header( 'Location: http://www.lovelydoggos.com');
+// link to redirect to
+header( 'Location: http://www.example.com');
+
 ?>
